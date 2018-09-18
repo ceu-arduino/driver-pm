5f47c68200f4558f2ebc9089b9c3e840137155c7
Francisco Sant'Anna <francisco.santanna@gmail.com>
2018-09-18 12:41:44 -0300

    (>) fallback to bitvector

--- pm.c
+++ pm.c
@@ -1,23 +1,23 @@
-#define CEU_PM_ON(dev) ((int)(ceu_pm_state[dev] > 0))
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-static u8 ceu_pm_state[CEU_PM_N];
+static u32 ceu_pm_state = 0;    // TODO: max 32 devices
 
 void ceu_pm_init (void);
 
-void _ceu_pm_init (void) {
-    memset(ceu_pm_state, 0, CEU_PM_N * sizeof(u8));
-}
-
 void ceu_pm_sleep (void);
 
-void ceu_pm_inc (u8 dev, bool inc) {
-    //ceu_assert(inc==-1 && ceu_pm_state[dev]>0 ||
-               //inc== 1 && ceu_pm_state[dev]<U8_MAX, "bug found");
-    ceu_pm_state[dev] += inc;
+int ceu_pm_get (int dev) {
+    return bitRead(ceu_pm_state, dev);
+}
+
+void ceu_pm_set (u8 dev, bool v) {
+    if (v) {
+        bitSet(ceu_pm_state, dev);
+    } else {
+        bitClear(ceu_pm_state, dev);
+    }
 }
 
 #ifdef __cplusplus
